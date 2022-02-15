bbbf8250ba31db31bfce747a8ea732eb8a545301
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-15 11:48:00 -0800

    added darkmode

--- redux-spotlight/src/views/Counter/Counter.js
+++ redux-spotlight/src/views/Counter/Counter.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment, multiply, reset } from './counterSlice';
+import {
+  decrement,
+  increment,
+  multiply,
+  reset,
+  DarkMode,
+} from './counterSlice';
+import './Counter.css';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
-
+  const isDarkMode = useSelector((state) => state.counter.isDarkMode);
+  console.log(isDarkMode);
   return (
-    <div>
+    <div className={isDarkMode ? 'dark' : 'day'}>
+      <button onClick={() => dispatch(DarkMode())}>darkmode</button>
       <button
         aria-label="Decrement value"
         onClick={() => {
